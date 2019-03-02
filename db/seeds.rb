06e329046861c173aa17fbbe67e4ee5c779af785
firedial <42674772+firedial@users.noreply.github.com>
2019-03-02 15:46:51 +0900

    create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+Kind.create!(name: 'test')
+Kind.create!(name: 'hoge')
+Kind.create!(name: 'huga')
+
+Purpose.create!(name: 'test')
+Purpose.create!(name: 'hoge')
+Purpose.create!(name: 'huga')
+
+Place.create!(name: 'test')
+Place.create!(name: 'hoge')
+Place.create!(name: 'huga')
+
+kinds = Kind.all
+purposes = Purpose.all
+places = Place.all
+
+Balance.create!(amount: 300, item: 'test', kind: kinds.find(1), purpose: purposes.find(1), place: places.find(1), date: '2019/2/2')
+Balance.create!(amount: 400, item: 'hoge', kind: kinds.find(1), purpose: purposes.find(2), place: places.find(1), date: '2019/2/3')
+Balance.create!(amount: 500, item: 'huga', kind: kinds.find(2), purpose: purposes.find(3), place: places.find(1), date: '2019/2/4')
